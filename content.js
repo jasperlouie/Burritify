d22f4855fd84f6c588608997a089849ce7a94dbd
Jasper Louie <jlouie95@gmail.com>
2015-10-10 02:50:59 -0700

    cleaned code

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 var elements = document.getElementsByTagName('*');
-var pattern = /\$\d+\.?\d*/;///\$\d+/;
+var pattern = /\$\d+\.?\d*/;
 var burrito_price = 8.25;
 
 for (var i = 0; i < elements.length; i++) {
@@ -13,24 +13,13 @@ for (var i = 0; i < elements.length; i++) {
             var result = pattern.exec(text);
             if (result != null) {
                 result = result[0];
-                //var amount = 5.0;
                 var amount = parseFloat(result.slice(1));
-                // var amount = 1;
-                var num_burritos = Math.round(amount/burrito_price*10000)/10000;
+                var num_burritos = Math.round(amount/burrito_price*100)/100;
                 var replacedText = text.replace(result, num_burritos.toString()+" burritos");
-                // replacedText = "as;ldkfja;lsdfj";
                 if (replacedText !== text) {
-            //     element.replaceChild(document.createTextNode(replacedText), node);
                 element.replaceChild(document.createTextNode(replacedText), node);
                 }
-
             }
-            // var replacedText = text.replace('Millennials', 'snake people');
-            //
-            // if (replacedText !== text) {
-            //     element.replaceChild(document.createTextNode(replacedText), node);
-            //     // element.replaceChild(document.createTextNode(replacedText), node);
-            // }
         }
     }
 }
