4f9ca89ce99a37e12084059b2403ecca1b0196bd
Cale Horeff <choreff@gmail.com>
2015-10-10 01:42:52 -0700

    Work on regex for dollars

--- content.js
+++ content.js
@@ -1,4 +1,6 @@
 var elements = document.getElementsByTagName('*');
+var pattern = new RegExp("$\d*");
+var burrito_price = 8
 
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
@@ -8,12 +10,20 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace('Millennials', 'snake people');
-
-            if (replacedText !== text) {
+            var result = pattern.exec(text);
+            if (result != null) {
+                var amount = parseInt(result.slice(1));
+                var num_burritos = amount / burrito_price
+                var replacedText = text.replace(result, toString(num_burritos));
                 element.replaceChild(document.createTextNode(replacedText), node);
-                // element.replaceChild(document.createTextNode(replacedText), node);
+
             }
+            // var replacedText = text.replace('Millennials', 'snake people');
+            //
+            // if (replacedText !== text) {
+            //     element.replaceChild(document.createTextNode(replacedText), node);
+            //     // element.replaceChild(document.createTextNode(replacedText), node);
+            // }
         }
     }
 }
